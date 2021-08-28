83249501c8367f643a4556011a13e92910369192
Tung Nguyen <tungnguyen@x3english.com>
2021-08-28 09:25:29 +0700

    update

--- popup.js
+++ popup.js
@@ -1,19 +1,23 @@
 //change coins list in this array
+//coin's name - entry_target - capital
 const coinsList = [
     'BTC',
     'ETH',
-    'TLM-0.326_0.58',
-    'TCT-0.029_0.039',
-    'OGN-0.79_1.2',
-    'LINK-47_100',
+    'TLM-0.326_0.58-128.8',
+    'TCT-0.029_0.039-86',
+    'OGN-0.79_1.2-106.01',
+    'LINK-47_100-100',
     'USDT',
 ];
 
 const fetchCoinAPI = async () => {
+    let totalInterestAmount = 0;
+
     coinsList.forEach(async (coinNameString) => {
         const coinNameComponents = coinNameString.split('-');
-        const coinName = coinNameComponents[0];
-        const range = coinNameComponents[1] ? `(${coinNameComponents[1]})` : '';
+        // const coinName = coinNameComponents[0];
+        // const range = coinNameComponents[1] ? `(${coinNameComponents[1]})` : '';
+        let [coinName, range, capital] = coinNameComponents;
 
         let url = `https://min-api.cryptocompare.com/data/price?fsym=${coinName}&tsyms=USDT`;
         let fetchResult = await fetch(url);
@@ -21,49 +25,35 @@ const fetchCoinAPI = async () => {
         const currentPrice = jsonResult.USDT;
         
         let interest = 0;
+
         let interestElement = `<span></span>`;
         if (range) {
-            let [entry, tp] = coinNameComponents[1].split('_');
+            let [entry, tp] = range.split('_');
             if (!isNaN(entry)) {
                 interest = 100 * ( Number(currentPrice) - Number(entry) ) / entry;
             }
+            range = `(${entry}_${tp})`
+        } else {
+            range = ''
         }
         if (interest != 0) {
+            const interestAmount = (interest * Number(capital)/100).toFixed(2);
+            totalInterestAmount += Number(interestAmount);
             interestElement = interest > 0 ? 
-            `<span style="color: green;">+${interest.toFixed(2)}%</span>` :
-            `<span style="color: red;">${interest.toFixed(2)}%</span>`
+            `<span style="color: green;">+${interest.toFixed(2)}% |${interestAmount}$</span>` :
+            `<span style="color: red;">${interest.toFixed(2)}% |${interestAmount}$</span>`
         }
 
         document.getElementById(`currenciesList`).innerHTML += `
-        <h3><a target="blank" href="https://www.cryptocompare.com/coins/${coinName.toLowerCase()}/overview/USDT">${coinName}${range}:</a> <span style="color: blue;">$${currentPrice}</span> ${interestElement}</h3>`
+        <h3>
+        <a target="blank" href="https://www.cryptocompare.com/coins/${coinName.toLowerCase()}/overview/USDT">${coinName}${range}:</a> 
+        <span style="color: blue;">$${currentPrice}</span> ${interestElement}
+        </h3>`
     });
-    // // get gold price
-    // document.getElementById('goldVNDPrice').innerHTML = await fetchGoldPrice();
     // Exchange USD to VNĐ
     let VNDcurrency = await fetchExchange();
     document.getElementById('VNDPrice').innerHTML = formatMoney(VNDcurrency);
-}
-
-const fetchGoldPrice = async () => {
-    let myHeaders = new Headers();
-
-    myHeaders.append("x-access-token", "goldapi-3pc5dukj0u2i3t-io");
-    myHeaders.append("Content-Type", "application/json");
-
-    const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    let result = await fetch("https://www.goldapi.io/api/XAU/USDT", requestOptions).then(response => response.text())
-        .then(result => result);
-    if (typeof result == 'string') result = JSON.parse(result);
-    if (typeof result == 'object') {
-        result = result.price;
-        result = result.toFixed(2);
-    }
-    return result;
+    document.getElementById('totalInterest').innerHTML = `${totalInterestAmount.toFixed(2)}$`;
 }
 
 const fetchExchange = async () => {
